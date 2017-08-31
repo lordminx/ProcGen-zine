2c44c283a09492fe9a468b585f2c16f9280f01ce
Jakob Bleier <themoep@themoep.at>
2017-08-31 13:32:44 +0200

    Added Colour Generator

---
+++ generators/colourgen.py
@@ -0,0 +1,13 @@
+from .default import DefaultGenerator
+
+
+class ColourGen(DefaultGenerator):
+
+    def generate(self):
+        # select number from #000000 to #ffffff to represent colour
+        colourNumber = self.random.randint(0,0xffffff)
+
+        # convert random number to colour representation string
+        colourString = hex(colourNumber)[2:]
+
+        return colourString
